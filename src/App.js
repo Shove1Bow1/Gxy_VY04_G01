27d0350b8025efeef8b74d0b12d12170ba6ef3b9
Shov1bow1 <fireblaster2001@gmail.com>
2022-04-14 12:30:37 +0700

    Hoang commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import {React} from "react";
 import { BrowserRouter as Router,Routes,Route  } from "react-router-dom";
 import './App.css';
-import Confirm from "./components/Confirm";
-import DangKy from "./components/DangKy";
-import DangKyFist from "./components/DangKyFist";
-import DangNhapTell from "./components/DangNhapTell";
-import EmailOtp from "./components/EmailOtp";
+import PartnerRegister from "./PathComponents/components/PartnerRegister/Merge"
 const App = () => {
   return(
-    <Router>
-     <DangKy/>
-     <Routes>
-     <Route path='/'element={<DangKyFist/>}/>
-     <Route path='/next1'element={<DangNhapTell/>}/>
-     <Route path='/next2'element={<EmailOtp/>}/>
-     <Route path='/next3'element={<Confirm/>}/>
-     </Routes>
-    </Router>   
+    <PartnerRegister/>
   )
 }
 export default App;
