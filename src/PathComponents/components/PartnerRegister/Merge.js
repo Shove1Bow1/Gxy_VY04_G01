c105af862e8737a4f1732d080cec291177617bfa
Shov1bow1 <fireblaster2001@gmail.com>
2022-04-14 11:46:55 +0700

    doi ten thu muc va noi luu

---
+++ src/PathComponents/components/PartnerRegister/Merge.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router,Routes,Route  } from "react-router-dom";
+import '../../../App.css';
+import Confirm from "./Confirm";
+import DangKy from "./DangKy";
+import DangKyFist from "./DangKyFist";
+import DangNhapTell from "./DangNhapTell";
+import EmailOtp from "./EmailOtp";
+const Register=()=>{
+    return(
+        <Router>
+            <DangKy />
+            <Routes>
+                <Route path='/' element={<DangKyFist />} />
+                <Route path='/next1' element={<DangNhapTell />} />
+                <Route path='/next2' element={<EmailOtp />} />
+                <Route path='/next3' element={<Confirm />} />
+            </Routes>
+        </Router>   
+    )
+}
+export default Register;
